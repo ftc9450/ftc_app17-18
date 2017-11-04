8f63e20c084072d0b36a656e57244d8a7deac96b
Oksana <tkachoksn@gmail.com>
2017-11-04 17:01:48 -0400

    move file to auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutonomousRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutonomousRed.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
