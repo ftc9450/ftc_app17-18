e8c1f89f8e2e72e2f41233b4210d471505463242
Oksana <tkachoksn@gmail.com>
2017-11-09 14:30:42 -0500

    Elevator

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * Created by O on 10/28/2017.
+ */
+
+public class Elevator extends Subsystem{
+    private DcMotor elevatorMotor;
+    private double speed = 0.3;
+
+    public enum ElevatorState{
+        UP,DOWN
+    }
+    private ElevatorState state;
+
+    public Elevator(DcMotor elevatorMotor){
+        this.elevatorMotor = elevatorMotor;
+        state = ElevatorState.OFF;
+    }
+
+    public void setState(ElevatorState state){this.state= state;}
+    public ElevatorState getState(){return this.state;}
+
+    @Override
+    public void stop() {elevatorMotor.setPower(0);}
+
+    @Override
+    public void zeroSensors() {
+    }
+
+    @Override
+    public void loop() {
+        switch(state){
+            case UP:
+                elevatorMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                elevatorMotor.setPower(speed);
+                break;
+            case DOWN:
+                elevatorMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                elevatorMotor.setPower(speed);
+        }
+    }
+}
