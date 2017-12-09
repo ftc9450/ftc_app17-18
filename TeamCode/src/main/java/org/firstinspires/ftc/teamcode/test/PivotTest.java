a36a9bd9fe49073d3b4e56423311806ca89a1233
dmanikt <dmanikt@gmail.com>
2017-12-09 18:40:45 -0500

    Add elevator test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/PivotTest.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.util.Constants;
+
+import java.util.Locale;
+
+/**
+ * Created by dhruv on 12/9/17.
+ */
+
+/**
+ * Simple test to drive forward and pivot 180° while driving
+ */
+
+public class PivotTest extends OpMode {
+    private Drivetrain drive;
+    private BNO055IMU imu;
+
+    @Override
+    public void init() {
+        /*BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit            = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit            = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile  = "IMUCalibration.json";
+        parameters.loggingEnabled       = true;
+        parameters.loggingTag           = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        drive = new Drivetrain(hardwareMap.dcMotor.get();*/
+    }
+
+    @Override
+    public void loop() {
+        ;
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+}
