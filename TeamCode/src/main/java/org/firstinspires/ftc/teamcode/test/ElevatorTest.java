f9c9b8b2d194de131db21caf01ffa05790d6bea8
kchen1250 <kchen1250@gmail.com>
2017-12-13 16:02:03 -0500

    Test glypht motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ElevatorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ElevatorTest.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.subsystems.Elevator;
 import org.firstinspires.ftc.teamcode.util.Constants;
@@ -10,22 +12,26 @@ import org.firstinspires.ftc.teamcode.util.Constants;
  * Created by dhruv on 12/9/17.
  */
 
+@TeleOp
 public class ElevatorTest extends OpMode {
-    private Elevator elevator;
+    private DcMotor elevator;
 
     @Override
     public void init() {
-        elevator = new Elevator(hardwareMap.dcMotor.get("glyph"));
+        elevator = hardwareMap.dcMotor.get("glyph");
     }
 
     @Override
     public void loop() {
-        switch (Math.round(gamepad1.left_stick_x)) {
-            case 1:
-                elevator.setState(Elevator.ElevatorState.UP);
-                break;
+        telemetry.addData("Left stick", gamepad1.left_stick_y);
+        switch (Math.round(gamepad1.left_stick_y)) {
             case -1:
-                elevator.setState(Elevator.ElevatorState.DOWN);
+                elevator.setDirection(DcMotorSimple.Direction.FORWARD);
+                elevator.setPower(1.0f);
+                break;
+            case 1:
+                elevator.setDirection(DcMotorSimple.Direction.REVERSE);
+                elevator.setPower(1.0f);
                 break;
             default:
                 break;
