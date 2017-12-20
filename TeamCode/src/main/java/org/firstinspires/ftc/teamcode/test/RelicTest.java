fc029e8ce4d8dd2098940df5124bb82cff3ef31a
dmanikt <dmanikt@gmail.com>
2017-12-20 17:02:03 -0500

    Test for Relic Arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RelicTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by dhruv on 12/20/17.
+ */
+
+@TeleOp
+public class RelicTest extends OpMode {
+    private Servo pivot;
+    private Servo hand;
+
+    @Override
+    public void init() {
+        pivot = hardwareMap.servo.get("pivot");
+        hand = hardwareMap.servo.get("hand");
+
+        pivot.setDirection(Servo.Direction.FORWARD);
+        hand.setDirection(Servo.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+        pivot.setPosition(Servo.MAX_POSITION);
+        hand.setPosition(Servo.MAX_POSITION);
+    }
+}
