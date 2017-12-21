64880a99bffe6d85271bc43a036009590637feb9
g-tang <gracetang021@gmail.com>
2017-12-20 19:58:56 -0500

    Fixed and functional RelicTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RelicTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RelicTest.java
@@ -3,29 +3,38 @@ package org.firstinspires.ftc.teamcode.test;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by dhruv on 12/20/17.
  */
 
-@Autonomous
+@TeleOp
 public class RelicTest extends OpMode {
-    private Servo pivot;
+    private CRServo pivot;
     private Servo hand;
 
     @Override
     public void init() {
-        pivot = hardwareMap.servo.get("pivot");
+        pivot = hardwareMap.crservo.get("pivot");
         hand = hardwareMap.servo.get("hand");
 
-        pivot.setDirection(Servo.Direction.FORWARD);
+        pivot.setDirection(CRServo.Direction.FORWARD);
         hand.setDirection(Servo.Direction.FORWARD);
     }
 
     @Override
     public void loop() {
-        pivot.setPosition(Servo.MAX_POSITION);
-        hand.setPosition(Servo.MAX_POSITION);
+        if(gamepad1.y){
+            pivot.setPower(1);
+        }else if(gamepad1.a){
+            pivot.setPower(-1);
+        }else{pivot.setPower(0);}
+        if(gamepad1.x) {
+            hand.setPosition(0);
+        }else if(gamepad1.b){
+            hand.setPosition(0.75);
+        }
     }
 }
