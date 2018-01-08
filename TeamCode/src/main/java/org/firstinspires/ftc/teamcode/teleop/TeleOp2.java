15f626721a18e3916d54e5232b38d2b4ee5498cf
dmanikt <dmanikt@gmail.com>
2018-01-08 13:27:58 -0500

    Fix grabber initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp2.java
@@ -39,7 +39,7 @@ public class TeleOp2 extends LinearOpMode{
                 new Grabber(hardwareMap.servo.get(Constants.Grabber.LB),hardwareMap.servo.get(Constants.Grabber.RB))};*/
         topGrabber=new Grabber(hardwareMap.servo.get(Constants.Grabber.LT),hardwareMap.servo.get(Constants.Grabber.RT));
         bottomGrabber=new Grabber(hardwareMap.servo.get(Constants.Grabber.LB),hardwareMap.servo.get(Constants.Grabber.RB));
-        arm = new RelicArm(hardwareMap.dcMotor.get("relic"), hardwareMap.crservo.get("pivot"), hardwareMap.crservo.get("pivot"), hardwareMap.servo.get("hand"));
+        arm = new RelicArm(hardwareMap.dcMotor.get("relic"), hardwareMap.crservo.get("l_pivot"), hardwareMap.crservo.get("r_pivot"), hardwareMap.servo.get("hand"));
         manager = new SubsystemManager();
         manager.add(drive);
         manager.add(topGrabber);
