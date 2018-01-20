3aea72bc350e2acb525e03f46b4811804e635c3e
dmanikt <dmanikt@gmail.com>
2018-01-20 18:03:10 -0500

    Practice code from Sanjana

---
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/subsystems/Drivetrain.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.team9450.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import java.util.HashMap;
+
+/**
+ * Created by dhruv on 1/20/18.
+ */
+
+public class Drivetrain extends Subsystem {
+    private DcMotor leftFront;
+    private DcMotor leftBack;
+    private DcMotor rightFront;
+    private DcMotor rightBack;
+
+    public Drivetrain(DcMotor lf, DcMotor lb, DcMotor rf, DcMotor rb) {
+        leftFront = lf;
+        leftBack = lb;
+        rightFront = rf;
+        rightBack = rb;
+
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void setPower(double driveSignal[]) {
+        leftFront.setPower(driveSignal[0]);
+        leftBack.setPower(driveSignal[1]);
+        rightFront.setPower(driveSignal[2]);
+        rightBack.setPower(driveSignal[3]);
+    }
+
+    public void setFWPosition(double pos) {
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    @Override
+    public void stop() {
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
---
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/test/RampTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.team9450.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by dhruv on 1/20/18.
+ */
+@TeleOp
+public class RampTest extends OpMode{
+    Servo servo;
+
+    @Override
+    public void init() {
+        servo = hardwareMap.servo.get("ramp");
+        servo.setDirection(Servo.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.a){
+            servo.setPosition(1);
+        }
+        else if(gamepad1.b){
+            servo.setPosition(0);
+        }
+
+    }
+}
