cf6e7d4ddb752d2c3c384a5471baa330bc558b53
dmanikt <dmanikt@gmail.com>
2018-01-22 13:15:08 -0500

    Vector wrapper class for robot movement

---
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/util/Vector2D.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.team9450.util;
+
+/**
+ * Created by dhruv on 1/22/18.
+ */
+
+public class Vector2D {
+    public double x, y;
+
+    public Vector2D(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double[] rotate(double angle) {
+        double temp = x;
+        x = Math.cos(angle)*x - Math.sin(angle)*y;
+        y = Math.sin(angle)*temp + Math.cos(angle)*y;
+        return new double[]{x, y};
+    }
+}
