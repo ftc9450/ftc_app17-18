3596ae390755c6b54b8b27ea682f60a1110b2aa7
manipabba <manipabba1@gmail.com>
2018-01-25 15:29:03 -0500

    made ramp into one class

--- Team9450/src/main/java/org/firstinspires/ftc/team9450/subsystems/Ramp.java
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/subsystems/Ramp.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.team9450.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.team9450.util.Constants;
 
 /**
  * Created by dhruv on 1/20/18.
@@ -8,23 +11,38 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Ramp extends Subsystem {
     private Servo servo;
-
+    DcMotor rampMotor;
+    private double speed= Constants.RampLifter.power;
     public enum RampState {
         IN, OUT
     }
+    public enum RampLifterState{
+        UP, DOWN, OFF
+    }
 
     private RampState state;
+    private RampLifterState liftState;
 
-    public Ramp(Servo ramp) {
+    public void setRampState(RampState state){
+        this.state = state;
+    }
+    public void setRampLifterState(RampLifterState state){
+        this.liftState = state;
+    }
+    public Ramp(Servo ramp, DcMotor motor) {
         this.servo = ramp;
         servo.setDirection(Servo.Direction.REVERSE);
         this.setState(RampState.OUT);
+        rampMotor = motor;
+        this.setRampLifterState(RampLifterState.DOWN);
     }
 
     @Override
     public void stop() {
         servo.setPosition(servo.getPosition());
+        rampMotor.setPower(0);
     }
+
     public void setState(RampState state) {
         this.state = state;
     }
@@ -41,5 +59,19 @@ public class Ramp extends Subsystem {
             default:
                 stop();
         }
+        switch (liftState) {
+            case UP:
+                if(rampMotor.getCurrentPosition()< Constants.RampLifter.maxPos) {
+                    rampMotor.setPower(speed);
+                }else{stop();}
+                break;
+            case DOWN:
+                if(rampMotor.getCurrentPosition()>=0) {
+                    rampMotor.setPower(-1 * speed);
+                }else{stop();}
+                break;
+            case OFF:
+        }
+
     }
 }
