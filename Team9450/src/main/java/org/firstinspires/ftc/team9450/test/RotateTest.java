ad7634d71daa6a0773aa56f67d4d3eb27973c4c7
dmanikt <dmanikt@gmail.com>
2018-01-28 12:58:23 -0500

    Rotation Testing

--- Team9450/src/main/java/org/firstinspires/ftc/team9450/test/RotateTest.java
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/test/RotateTest.java
@@ -2,31 +2,25 @@ package org.firstinspires.ftc.team9450.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.team9450.sensors.Gyroscope;
 import org.firstinspires.ftc.team9450.subsystems.Drivetrain;
 import org.firstinspires.ftc.team9450.util.Constants;
-import org.firstinspires.ftc.team9450.util.DriveSignal;
 
 /**
  * Created by dhruv on 1/28/18.
  */
 
 @Autonomous
-public class PathTest extends LinearOpMode {
+public class RotateTest extends LinearOpMode {
     Drivetrain drive;
-    Gyroscope imu;
     @Override
     public void runOpMode() throws InterruptedException {
         drive = new Drivetrain(hardwareMap.dcMotor.get(Constants.Drivetrain.LF), hardwareMap.dcMotor.get(Constants.Drivetrain.LB), hardwareMap.dcMotor.get(Constants.Drivetrain.RF), hardwareMap.dcMotor.get(Constants.Drivetrain.RB));
-        drive.enableAndResetEncoders();
-        waitForStart();
-        drive.moveFB(10, 1);
         drive.disconnectEncoders();
-        while (imu.getAngle() < Math.PI/2) {
-            drive.setOpenLoop(new DriveSignal(1, 1, -1, -1));
+        waitForStart();
+        while (opModeIsActive()) {
+            drive.setPower(new double[]{0.3, 0.3, -0.3, -0.3});
         }
-        drive.enableAndResetEncoders();
-        drive.moveFB(10, 1);
     }
 }
