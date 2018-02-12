e5f51df620d34cc2e5b23d7cd2a28f82d9c200da
dmanikt <dmanikt@gmail.com>
2018-02-12 17:28:23 -0500

    Add BNO055IMU localizer

--- Team9450/src/main/java/org/firstinspires/ftc/team9450/test/AccelerometerTest.java
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/test/AccelerometerTest.java
@@ -4,18 +4,19 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.team9450.sensors.Accelerometer;
+import org.firstinspires.ftc.team9450.sensors.Localizer;
 
 /**
  * Created by dhruv on 2/10/18.
  */
 
 public class AccelerometerTest extends OpMode {
-    private Accelerometer imu;
+    private Localizer imu;
     private DriveTest dt;
 
     @Override
     public void init() {
-        imu = new Accelerometer(hardwareMap.get(BNO055IMU.class, "imu"));
+        imu = new Localizer(hardwareMap.get(BNO055IMU.class, "imu"));
     }
 
     @Override
