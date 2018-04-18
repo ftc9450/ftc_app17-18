7e8384c1de484506d10b3920e54afe7258146a5f
dmanikt <dmanikt@gmail.com>
2018-04-18 16:55:04 -0400

    Ramp Calibration

--- Team9450/src/main/java/org/firstinspires/ftc/team9450/util/Constants.java
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/util/Constants.java
@@ -43,9 +43,9 @@ public class Constants {
         public static final String RAMP = "ramp";
         public static final String LIFT = "lift";
         public static final int MAX = 4600;
-        public static final double IN=1.0;
-        public static final double OUT=0.3;
-        public static final double LEVEL = 0.94;
+        public static final double IN = 0.3;
+        public static final double OUT = 0.8;
+        public static final double LEVEL = 0.3;
     }
     public class Setup {
         public static final String VUFORIA_KEY = "AYwm7lb/////AAAAGeQI9HT4B0R2unLNBq/DsId49BJr71nKGdfP8X8fnmtD0Jna47KLigPBytLYBjzOIl6uCfYWbIXHc3FqoabxIITohKJ4VvPispe5kGGFFJyQEIifEL1Bc511jOl00pyY2Tr/YOGwjGk7lSXQ0QrScHVaiwIOM3mUUlsv9Ethn1OCZB2AVhT91gnrUKryxBwfLCGjqpmYdWOVDsJTloDiowWMez0U42S9sILVevNguQXZqTr1uURaUx5Voy+2N6FVK5p4dvraac9+LD6YskUCLqWsK2XVruCpCsRWZxfrqylNyni2ll5AW3Mekw/hSSzfjx70eyKXyaLRiOj4UhHKCjeqWjFCePt0Vb59tyqd9KhS";
